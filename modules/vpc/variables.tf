8217eacd2da07e2a2d2ff8ce73cee457ad04c66e
Ulises Lárraga <uliseslarraga@gmail.com>
2025-07-16 10:35:10 -0600

    feat: add VPC module

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,48 @@
+data "aws_availability_zones" "available_zones" {
+  state = "available"
+}
+
+variable "tags" {
+  type        = any
+  description = "Common resource tags"
+}
+
+variable "environment" {
+  type        = string
+  description = "Env name"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR to be used for the new VPC"
+}
+
+variable "public_subnet_count" {
+  description = "Number of public subnets"
+  type        = number
+  default     = 2
+  validation {
+    condition     = var.public_subnet_count >= 1 && var.public_subnet_count <= 6
+    error_message = "Public subnet count must be between 1 and 6."
+  }
+}
+
+variable "private_subnet_count" {
+  description = "Number of private subnets"
+  type        = number
+  default     = 2
+  validation {
+    condition     = var.private_subnet_count >= 1 && var.private_subnet_count <= 6
+    error_message = "Private subnet count must be between 1 and 6."
+  }
+}
+
+variable "data_subnet_count" {
+  description = "Number of data subnets"
+  type        = number
+  default     = 2
+  validation {
+    condition     = var.data_subnet_count >= 1 && var.data_subnet_count <= 6
+    error_message = "Private subnet count must be between 1 and 6."
+  }
+}
